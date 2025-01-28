5c6b271f6e21d090383e073efae711c0f9735fed
Andrew Ignácio <andrewignacio@outlook.com.br>
2025-01-28 11:54:43 -0300

    fix: change exercise 03 logic

--- src/exercise03/Program.java
+++ src/exercise03/Program.java
@@ -1,5 +1,7 @@
 /*
  * Faça um algoritmo que leia dois valores inteiros A e B, se os valores de A e B forem iguais, deverá somar os dois valores,
+ * caso contrário devera multiplicar A por B. Ao final de qualquer um dos cálculos deve-se atribuir o resultado a uma variável C e
+ * imprimir seu valor na tela.
  * */
 package exercise03;
 
@@ -15,9 +17,14 @@ public class Program {
         System.out.print("B: ");
         int b = scanner.nextInt();
 
+        int c;
+
         if (a == b) {
-            int sum = a + b;
-            System.out.printf("%d + %d = %d", a, b, sum);
+            c = a + b;
+        } else {
+            c = a * b;
         }
+
+        System.out.printf("C: %d", c);
     }
 }
